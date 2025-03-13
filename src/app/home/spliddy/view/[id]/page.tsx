2fa0272f530a3cc44025ff9624a6f56f9d30416a
Mohit Raghuwanshi <raghuwanshimohit1@gmail.com>
2025-03-13 18:57:33 +0530

    logout issue

--- src/app/home/spliddy/view/[id]/page.tsx
+++ src/app/home/spliddy/view/[id]/page.tsx
@@ -1,10 +1,16 @@
 import SpliddyCreator from "@/components/SpliddyCreator";
 import { PrismaClient } from "@prisma/client";
+import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
 
 const client = new PrismaClient();
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+    const session = await getServerSession();
+
+    if (!session) {
+        redirect('/');
+    }
     const { id } = (await params)
 
     const data = await client.spliddies.findFirst({
